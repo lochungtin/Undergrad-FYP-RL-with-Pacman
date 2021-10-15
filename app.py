2c55536f4b9d7695c96b77b01119efa407c276ac
lochungtin <lochungtin@gmail.com>
2021-10-15 01:06:54 +0100

    rm flicker + smooth movement

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
+from threading import *
 from tkinter import *
+import time
 
 from constants import *
 from game import Game
@@ -6,79 +8,39 @@ from game import Game
 
 class App:
     def __init__(self):
+        # initialise game object
+        self.game = Game()
+
         # create application object
         self.main = Tk()
         # bind key hanlders
-        self.main.bind('<Up>', self.keyUp)
-        self.main.bind('<Down>', self.keyDown)
-        self.main.bind('<Left>', self.keyRight)
-        self.main.bind('<Right>', self.keyLeft)
-        self.main.bind('<space>', self.keySpace)
+        self.main.bind("<Up>", lambda _: self.handleKey(DIR.UP))
+        self.main.bind("<Down>", lambda _: self.handleKey(DIR.DW))
+        self.main.bind("<Left>", lambda _: self.handleKey(DIR.LF))
+        self.main.bind("<Right>", lambda _: self.handleKey(DIR.RT))
+        self.main.bind("<space>", lambda _: self.game.togglePause())
+        # handle kill event
+        self.main.protocol("WM_DELETE_WINDOW", self.kill)
         # create canvas object
         self.canvas = Canvas(
             self.main,
             width=DIM.GBL_W,
             height=DIM.GBL_H,
-            background='#1E1E1E',
+            background="#1E1E1E",
             highlightthickness=0,
         )
         self.canvas.pack()
 
-        # initialise game object
-        self.game = Game()
-
-    # keystroke handlers
-    def keyUp(self, event):
-        self.handleKey(DIR.UP)
-
-    def keyDown(self, event):
-        self.handleKey(DIR.DW)
-
-    def keyRight(self, event):
-        self.handleKey(DIR.RT)
-
-    def keyLeft(self, event):
-        self.handleKey(DIR.LF)
-
-    def keySpace(self, event):
-        print('S')
-
-    def handleKey(self, keyID):
-        self.game.pacman.setDir(keyID)
-        self.game.nextState()
-
-        self.updateCanvas()
-
-    # update canvas
-    def updateCanvas(self):
-        # clear
-        self.canvas.delete('all')
-
+        # draw grid
         for row in range(BOARD.row):
             for col in range(BOARD.col):
-                rep = self.game.getState()[row][col]
+                rep = self.game.state[row][col]
 
-                positioner = DIM.CELL + DIM.GAP
-                x0 = col * positioner
-                y0 = row * positioner
-                x1 = x0 + DIM.CELL
-                y1 = y0 + DIM.CELL
+                x0, y0, x1, y1 = self.calPxPos(row, col)
 
-                if rep == REP.PWRPLT:
+                if rep == 2:
                     self.canvas.create_rectangle(
-                        x0, y0, x1, y1, fill=REP.COLOR_MAP[REP.EMPTY], outline=''
-                    )
-                    self.canvas.create_rectangle(
-                        x0 + DIM.PAD_PWRPLT,
-                        y0 + DIM.PAD_PWRPLT,
-                        x1 - DIM.PAD_PWRPLT,
-                        y1 - DIM.PAD_PWRPLT,
-                        fill=REP.COLOR_MAP[rep],
-                        outline='',
-                    )
-                elif rep == REP.DOOR:
-                    self.canvas.create_rectangle(
-                        x0, y0, x1, y1, fill=REP.COLOR_MAP[REP.EMPTY], outline=''
+                        x0, y0, x1, y1, fill=REP.COLOR_MAP[REP.EMPTY], outline=""
                     )
                     self.canvas.create_rectangle(
                         x0,
@@ -86,16 +48,62 @@ class App:
                         x1,
                         y1 - DIM.PAD_DOOR,
                         fill=REP.COLOR_MAP[rep],
-                        outline='',
+                        outline="",
                     )
                 else:
                     self.canvas.create_rectangle(
-                        x0, y0, x1, y1, fill=REP.COLOR_MAP[rep], outline=''
+                        x0, y0, x1, y1, fill=REP.COLOR_MAP[rep], outline=""
                     )
+
+        # draw pacman
+        pacmanRow, pacmanCol = POS.PACMAN
+        self.game.pacman.setDiplayObj(
+            self.canvas.create_rectangle(
+                self.calPxPos(pacmanRow, pacmanCol),
+                fill=REP.COLOR_MAP[REP.PACMAN],
+                outline="",
+            )
+        )
+        self.game.setState(pacmanRow, pacmanCol, REP.PACMAN)
+
+        # start update loop
+        self.running = True
+        Thread(target=self.tCtrl).start()
+
+    # calculate pixel positions
+    def calPxPos(self, row, col):
+        positioner = DIM.CELL + DIM.GAP
+
+        x0 = col * positioner
+        y0 = row * positioner
+
+        return x0, y0, x0 + DIM.CELL, y0 + DIM.CELL
+
+    # keystroke handlers
+    def handleKey(self, keyID):
+        self.game.pacman.setDir(keyID)
+
+        print("pressed: {}".format(keyID))
+
+    # update canvas
+    def updateCanvas(self):
+        dx, dy = self.game.pacman.getDisplayDelta()
+        self.canvas.move(self.game.pacman.diplay, dx, dy)
+        return
+
+    # time controller
+    def tCtrl(self):
+        while self.running:
+            time.sleep(0.1)
+
+            self.game.nextState()
+            self.updateCanvas()
+
+    def kill(self):
+        self.running = False
+        self.main.destroy()
 
     # start app
     def run(self):
         # run main loop of application
-        self.updateCanvas()
-        self.game.nextState()
         self.main.mainloop()
--- pacman.py
+++ pacman.py
@@ -7,8 +7,19 @@ class Pacman:
         self.prevPos = (x, y)
         self.direction = DIR.UP
 
+        self.calDisplay = False
+        self.displayDx = 0
+        self.displayDy = 0
+
     def setDir(self, direction):
         self.direction = direction
+
+    def setDiplayObj(self, obj):
+        self.diplay = obj
+        self.calDisplay = True
+
+    def getDisplayDelta(self):
+        return self.displayDx, self.displayDy
 
     def isValidPos(self, pos):
         return pos[1] > -1 and pos[1] < BOARD.col and pos[0] > -1 and pos[0] < BOARD.row
@@ -16,23 +27,42 @@ class Pacman:
     def nextPos(self, state):
         if self.direction == DIR.UP:
             nextPos = (self.pos[0] - 1, self.pos[1])
+            self.displayDx = 0
+            self.displayDy = -(DIM.CELL + DIM.GAP)
+
         elif self.direction == DIR.DW:
             nextPos = (self.pos[0] + 1, self.pos[1])
+            self.displayDx = 0
+            self.displayDy = (DIM.CELL + DIM.GAP)
+
         elif self.direction == DIR.RT:
             nextPos = (self.pos[0], self.pos[1] - 1)
+            self.displayDx = (DIM.CELL + DIM.GAP)
+            self.displayDy = 0
+
         else:
             nextPos = (self.pos[0], self.pos[1] + 1)
+            self.displayDx = -(DIM.CELL + DIM.GAP)
+            self.displayDy = 0
 
         # loop case left
         if nextPos == (14, -1):
             self.prevPos = (14, 0)
             self.pos = (14, 26)
+
+            self.displayDx = -(DIM.CELL + DIM.GAP) * 26
+            self.displayDy = 0
+
             return self.prevPos, self.pos
 
         # loop case right
         if nextPos == (14, 27):
             self.prevPos = (14, 26)
             self.pos = (14, 0)
+
+            self.displayDx = (DIM.CELL + DIM.GAP) * 26
+            self.displayDy = 0
+
             return self.prevPos, self.pos
         
         # default movement
@@ -43,5 +73,8 @@ class Pacman:
         ):
             self.prevPos = self.pos
             self.pos = nextPos
+        else:
+            self.displayDx = 0
+            self.displayDy = 0
 
         return self.prevPos, self.pos
