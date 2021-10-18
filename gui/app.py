38d6a35284b960f0c1274b1fb978914bdd092ced
lochungtin <lochungtin@gmail.com>
2021-10-18 14:53:47 +0100

    draw canvas

--- gui/app.py
+++ gui/app.py
@@ -1,7 +1,8 @@
-from threading import *
+from threading import Thread
 from tkinter import *
 import sys
 import time
+from typing import Tuple
 
 from data import DIM, DIR, REP
 from game.game import Game
@@ -10,10 +11,10 @@ from game.game import Game
 class App:
     def __init__(self) -> None:
         # create game object
-        self.game = Game()
+        self.game: Game = Game()
 
         # create application
-        self.main = Tk()
+        self.main: Tk = Tk()
         self.main.title("Pacman")
 
         # bind key hanlders
@@ -27,7 +28,7 @@ class App:
         self.main.protocol("WM_DELETE_WINDOW", self.kill)
 
         # create canvas object
-        self.canvas = Canvas(
+        self.canvas: Canvas = Canvas(
             self.main,
             width=DIM.GBL_W,
             height=DIM.GBL_H,
@@ -40,13 +41,63 @@ class App:
         self.initialGame()
 
         # start update loop
-        self.playing = True
-        self.running = True
-        Thread(target=self.controller).start()
+        self.playing: bool = True
+        self.running: bool = True
+        # Thread(target=self.controller).start()
+
+    # calculate pixel position of grid
+    def calculatePos(self, row, col) -> Tuple[int, int, int, int]:
+        x0: int = col * DIM.JUMP
+        y0: int = row * DIM.JUMP
+        return x0, y0, x0 + DIM.CELL, y0 + DIM.CELL
 
     # create canvas objects from displayable list
     def initialGame(self):
-        pass
+        # draw grid
+        for rowIndex, stateRow in enumerate(self.game.state):
+            for colIndex, cell in enumerate(stateRow):
+                x0, y0, x1, y1 = self.calculatePos(rowIndex, colIndex)
+
+                if REP.isPellet(cell):
+                    # draw pellets
+                    if cell == REP.PELLET:
+                        canvasItemId: int = self.canvas.create_rectangle(
+                            x0 + DIM.PAD_PELLET,
+                            y0 + DIM.PAD_PELLET,
+                            x1 - DIM.PAD_PELLET,
+                            y1 - DIM.PAD_PELLET,
+                            fill=REP.COLOR_MAP[cell],
+                            outline="",
+                        )
+                    # draw power pellet
+                    else:
+                        canvasItemId: int = self.canvas.create_rectangle(
+                            x0 + DIM.PAD_PWRPLT,
+                            y0 + DIM.PAD_PWRPLT,
+                            x1 - DIM.PAD_PWRPLT,
+                            y1 - DIM.PAD_PWRPLT,
+                            fill=REP.COLOR_MAP[cell],
+                            outline="",
+                        )
+
+                    # add canvas item id to pellet object
+                    self.game.pellets[rowIndex][colIndex].setCanvasItemId(canvasItemId)
+                elif cell != REP.EMPTY:
+                    canvasItemId: int = self.canvas.create_rectangle(
+                        x0, y0, x1, y1, fill=REP.COLOR_MAP[cell], outline=""
+                    )
+
+                    # add canvas item id to movable
+                    if cell == REP.PACMAN:
+                        self.game.pacman.setCanvasItemId(canvasItemId)
+                    elif cell == REP.BLINKY:
+                        self.game.blinky.setCanvasItemId(canvasItemId)
+                    elif cell == REP.INKY:
+                        self.game.inky.setCanvasItemId(canvasItemId)
+                    elif cell == REP.CLYDE:
+                        self.game.clyde.setCanvasItemId(canvasItemId)
+                    elif cell == REP.PINKY:
+                        self.game.pinky.setCanvasItemId(canvasItemId)
 
     # time controller
     def controller(self) -> None:
