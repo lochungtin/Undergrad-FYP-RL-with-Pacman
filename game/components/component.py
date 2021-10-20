b416403e22acab0c1a4e122313a60e1682a3abc2
lochungtin <lochungtin@gmail.com>
2021-10-20 09:23:19 +0100

    additional precausions

--- game/components/component.py
+++ game/components/component.py
@@ -1,8 +1,9 @@
+from copy import deepcopy
 from gui.displayable import Displayable
 from utils.coordinate import CPair
 
 
 class Component(Displayable):
     def __init__(self, pos: CPair, repId: int) -> None:
-        self.pos: CPair = pos
+        self.pos: CPair = deepcopy(pos)
         self.repId: int = repId
