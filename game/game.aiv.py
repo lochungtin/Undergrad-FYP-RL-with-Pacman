a53694e77d3dd9949e9e4ab9de653f86270290ef
lochungtin <lochungtin@gmail.com>
2021-11-08 23:22:15 +0000

    remove disable pacman option

--- game/game.aiv.py
+++ game/game.aiv.py
@@ -2,5 +2,5 @@ from game.game import Game
 
 
 class AIV(Game):
-    def __init__(self,  enableGhost: bool = True, enablePwrPlt: bool = True) -> None:
-        super().__init__(enablePacman=True, enableGhost=enableGhost, enablePwrPlt=enablePwrPlt)
+    def __init__(self, enableGhost: bool = True, enablePwrPlt: bool = True) -> None:
+        super().__init__(enableGhost=enableGhost, enablePwrPlt=enablePwrPlt)
