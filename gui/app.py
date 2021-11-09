6d089a370209b0d1f3dbccf78514f8d719099faa
lochungtin <lochungtin@gmail.com>
2021-11-09 11:53:16 +0000

    game display abstraction

--- gui/app.py
+++ gui/app.py
@@ -5,6 +5,7 @@ import _thread
 from data import DIM, DIR, REP
 from game.game import Game
 from gui.controller import TimeController
+from gui.display import Display
 from gui.utils import GUIUtil
 
 
@@ -37,18 +38,8 @@ class App:
         # handle kill event
         self.main.protocol("WM_DELETE_WINDOW", self.kill)
 
-        # create canvas object
-        self.canvas: Canvas = Canvas(
-            self.main,
-            width=DIM.GBL_W,
-            height=DIM.GBL_H,
-            background=REP.COLOR_MAP[REP.BG],
-            highlightthickness=0,
-        )
-        self.canvas.pack()
-
-        # bind objects with canvas items
-        self.initialiseGame()
+        # create game display
+        self.display: Display = Display(self.game, self.main)
 
         # bind nextStep() controllers
         if manualControl:
@@ -56,129 +47,19 @@ class App:
         else:
             _thread.start_new_thread(self.timeController.start, ())
 
-    # create canvas objects from displayable list
-    def initialiseGame(self) -> None:
-        # draw grid
-        for rowIndex, stateRow in enumerate(self.game.state):
-            for colIndex, cell in enumerate(stateRow):
-                x0, y0, x1, y1 = GUIUtil.calculatePos(rowIndex, colIndex)
-
-                if REP.isPellet(cell):
-                    # draw pellets
-                    if cell == REP.PELLET:
-                        canvasItemId: int = self.canvas.create_rectangle(
-                            x0 + DIM.PAD_PELLET,
-                            y0 + DIM.PAD_PELLET,
-                            x1 - DIM.PAD_PELLET,
-                            y1 - DIM.PAD_PELLET,
-                            fill=REP.COLOR_MAP[cell],
-                            outline="",
-                        )
-                    # draw power pellet
-                    else:
-                        canvasItemId: int = self.canvas.create_rectangle(
-                            x0 + DIM.PAD_PWRPLT,
-                            y0 + DIM.PAD_PWRPLT,
-                            x1 - DIM.PAD_PWRPLT,
-                            y1 - DIM.PAD_PWRPLT,
-                            fill=REP.COLOR_MAP[cell],
-                            outline="",
-                        )
-
-                    # add canvas item id to pellet object
-                    self.game.pellets[rowIndex][colIndex].setCanvasItemId(canvasItemId)
-
-                elif cell != REP.EMPTY:
-                    if cell == REP.DOOR:
-                        canvasItemId: int = self.canvas.create_rectangle(
-                            x0,
-                            y0 + DIM.PAD_DOOR,
-                            x1,
-                            y1 - DIM.PAD_DOOR,
-                            fill=REP.COLOR_MAP[cell],
-                            outline="",
-                        )
-                    else:
-                        canvasItemId: int = self.canvas.create_rectangle(
-                            x0, y0, x1, y1, fill=REP.COLOR_MAP[cell], outline=""
-                        )
-
-                    # add canvas item id to movable
-                    if cell == REP.PACMAN:
-                        self.game.pacman.setCanvasItemId(canvasItemId)
-                    elif cell == REP.BLINKY:
-                        self.game.blinky.setCanvasItemId(canvasItemId)
-                    elif cell == REP.INKY:
-                        self.game.inky.setCanvasItemId(canvasItemId)
-                    elif cell == REP.CLYDE:
-                        self.game.clyde.setCanvasItemId(canvasItemId)
-                    elif cell == REP.PINKY:
-                        self.game.pinky.setCanvasItemId(canvasItemId)
-
-    # trigger Game.nextStep() and update canvas
-    # reset canvas if gameover
+    # trigger Game.nextStep() and update dislay, reset if gameover
     def nextStep(self):
         # update game, proceed to next step
         gameover, won, atePellet = self.game.nextStep()
 
-        # remove pellet
-        pPos = self.game.pacman.pos
-        if atePellet:
-            self.canvas.delete(self.game.pellets[pPos.row][pPos.col].canvasItemId)
-
-        # update pacman location
-        if self.game.pacman.moved:
-            pdX, pdY = GUIUtil.calculateDxDy(pPos, self.game.pacman.prevPos)
-            self.canvas.move(self.game.pacman.canvasItemId, pdX, pdY)
-
-        # ghost updates
-        if self.enableGhost:
-            for ghost in self.game.ghosts:
-                # delete old paths
-                if hasattr(ghost.prevPath, "canvasItemId"):
-                    self.canvas.delete(ghost.prevPath.canvasItemId)
-
-                # update path display
-                if not ghost.isFrightened:
-                    displayPath: List[int] = []
-                    for cpair in ghost.path.path:
-                        x, y = GUIUtil.calculateMidPt(cpair)
-                        displayPath.append(x)
-                        displayPath.append(y)
-
-                    if len(displayPath) > 2:
-                        ghost.path.setCanvasItemId(
-                            self.canvas.create_line(
-                                displayPath, width=3, fill=REP.COLOR_MAP[ghost.repId]
-                            )
-                        )
-
-                    # update color
-                    if ghost.isDead:
-                        self.canvas.itemconfig(
-                            ghost.canvasItemId, fill=REP.COLOR_MAP[REP.DEAD]
-                        )
-                    else:
-                        self.canvas.itemconfig(
-                            ghost.canvasItemId, fill=REP.COLOR_MAP[ghost.repId]
-                        )
-                else:
-                    self.canvas.itemconfig(
-                        ghost.canvasItemId, fill=REP.COLOR_MAP[REP.FRIGHTENED]
-                    )
-
-                # update location
-                if ghost.moved:
-                    dX, dY = GUIUtil.calculateDxDy(ghost.pos, ghost.prevPos)
-                    self.canvas.move(ghost.canvasItemId, dX, dY)
-
+        # handle gameover
         if gameover:
-            # clear canvas
-            self.canvas.delete("all")
-
-            # create new game and bind objects with canvas items
+            # create new game
             self.game = Game(self.enableGhost, self.enablePwrPlt)
-            self.initialiseGame()
+            self.display.newGame(self.game)
+
+        # update display
+        self.display.rerender(atePellet)
 
     # kill program
     def kill(self) -> None:
--- gui/display.py
+++ gui/display.py
@@ -1,9 +1,138 @@
+from tkinter import Canvas, Tk
+from typing import List
+from data import DIM, REP
 from game.game import Game
+from gui.utils import GUIUtil
 
 
 class Display:
-    def __init__(self, game: Game) -> None:
+    def __init__(self, game: Game, main: Tk) -> None:
         self.game: Game = game
 
-    def rerender(self) -> None:
-        return
+        # create canvas and add to window
+        self.canvas: Canvas = Canvas(
+            main,
+            width=DIM.GBL_W,
+            height=DIM.GBL_H,
+            background=REP.COLOR_MAP[REP.BG],
+            highlightthickness=0,
+        )
+        self.canvas.pack()
+
+        self.bindObjects()
+
+    def newGame(self, game: Game) -> None:
+        self.game = game
+
+        self.canvas.delete("all")
+        self.bindObjects()
+
+    def bindObjects(self) -> None:
+        # draw grid
+        for rowIndex, stateRow in enumerate(self.game.state):
+            for colIndex, cell in enumerate(stateRow):
+                x0, y0, x1, y1 = GUIUtil.calculatePos(rowIndex, colIndex)
+
+                if REP.isPellet(cell):
+                    # draw pellets
+                    if cell == REP.PELLET:
+                        canvasItemId: int = self.canvas.create_rectangle(
+                            x0 + DIM.PAD_PELLET,
+                            y0 + DIM.PAD_PELLET,
+                            x1 - DIM.PAD_PELLET,
+                            y1 - DIM.PAD_PELLET,
+                            fill=REP.COLOR_MAP[cell],
+                            outline="",
+                        )
+                    # draw power pellet
+                    else:
+                        canvasItemId: int = self.canvas.create_rectangle(
+                            x0 + DIM.PAD_PWRPLT,
+                            y0 + DIM.PAD_PWRPLT,
+                            x1 - DIM.PAD_PWRPLT,
+                            y1 - DIM.PAD_PWRPLT,
+                            fill=REP.COLOR_MAP[cell],
+                            outline="",
+                        )
+
+                    # add canvas item id to pellet object
+                    self.game.pellets[rowIndex][colIndex].setCanvasItemId(canvasItemId)
+
+                elif cell != REP.EMPTY:
+                    if cell == REP.DOOR:
+                        canvasItemId: int = self.canvas.create_rectangle(
+                            x0,
+                            y0 + DIM.PAD_DOOR,
+                            x1,
+                            y1 - DIM.PAD_DOOR,
+                            fill=REP.COLOR_MAP[cell],
+                            outline="",
+                        )
+                    else:
+                        canvasItemId: int = self.canvas.create_rectangle(
+                            x0, y0, x1, y1, fill=REP.COLOR_MAP[cell], outline=""
+                        )
+
+                    # add canvas item id to movable
+                    if cell == REP.PACMAN:
+                        self.game.pacman.setCanvasItemId(canvasItemId)
+                    elif cell == REP.BLINKY:
+                        self.game.blinky.setCanvasItemId(canvasItemId)
+                    elif cell == REP.INKY:
+                        self.game.inky.setCanvasItemId(canvasItemId)
+                    elif cell == REP.CLYDE:
+                        self.game.clyde.setCanvasItemId(canvasItemId)
+                    elif cell == REP.PINKY:
+                        self.game.pinky.setCanvasItemId(canvasItemId)
+
+    def rerender(self, atePellet: bool) -> None:
+        # remove pellet
+        pPos = self.game.pacman.pos
+        if atePellet:
+            self.canvas.delete(self.game.pellets[pPos.row][pPos.col].canvasItemId)
+
+        # update pacman location
+        if self.game.pacman.moved:
+            pdX, pdY = GUIUtil.calculateDxDy(pPos, self.game.pacman.prevPos)
+            self.canvas.move(self.game.pacman.canvasItemId, pdX, pdY)
+
+        # ghost updates
+        if self.game.enableGhost:
+            for ghost in self.game.ghosts:
+                # delete old paths
+                if hasattr(ghost.prevPath, "canvasItemId"):
+                    self.canvas.delete(ghost.prevPath.canvasItemId)
+
+                # update path display
+                if not ghost.isFrightened:
+                    displayPath: List[int] = []
+                    for cpair in ghost.path.path:
+                        x, y = GUIUtil.calculateMidPt(cpair)
+                        displayPath.append(x)
+                        displayPath.append(y)
+
+                    if len(displayPath) > 2:
+                        ghost.path.setCanvasItemId(
+                            self.canvas.create_line(
+                                displayPath, width=3, fill=REP.COLOR_MAP[ghost.repId]
+                            )
+                        )
+
+                    # update color
+                    if ghost.isDead:
+                        self.canvas.itemconfig(
+                            ghost.canvasItemId, fill=REP.COLOR_MAP[REP.DEAD]
+                        )
+                    else:
+                        self.canvas.itemconfig(
+                            ghost.canvasItemId, fill=REP.COLOR_MAP[ghost.repId]
+                        )
+                else:
+                    self.canvas.itemconfig(
+                        ghost.canvasItemId, fill=REP.COLOR_MAP[REP.FRIGHTENED]
+                    )
+
+                # update location
+                if ghost.moved:
+                    dX, dY = GUIUtil.calculateDxDy(ghost.pos, ghost.prevPos)
+                    self.canvas.move(ghost.canvasItemId, dX, dY)
