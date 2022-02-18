a48bd597e6b0c51c1a0085f3c1936b0e771481be
lochungtin <lochungtin@gmail.com>
2022-02-18 19:10:44 +0000

    auto viewer

--- auto.py
+++ auto.py
@@ -3,33 +3,37 @@ from typing import List
 import _thread
 import numpy as np
 
-from ai.deepq.neuralnet import NeuralNet
 from agents.pacman import PacmanDQLAgent
 from agents.blinky import BlinkyClassicAgent
 from agents.inky import InkyClassicAgent
 from agents.clyde import ClydeClassicAgent
 from agents.pinky import PinkyClassicAgent
-from data.data import POS, REP
+from ai.deepq.neuralnet import NeuralNet
+from ai.neat.genome import Genome
+from ai.neat.utils import GenomeUtils
 from game.game import Game
 from gui.controller import TimeController
 from gui.display import Display
 
 
 class App:
-    def __init__(self, filename) -> None:
+    def __init__(self, gameConfig: dict[str, object]) -> None:
         self.game = Game(
-            PacmanDQLAgent(NeuralNet.load(filename)),
-            blinky=BlinkyClassicAgent(),
-            pinky=PinkyClassicAgent()
+            gameConfig["agents"]["pacman"],
+            blinky=gameConfig["agents"]["blinky"],
+            inky=gameConfig["agents"]["inky"],
+            clyde=gameConfig["agents"]["clyde"],
+            pinky=gameConfig["agents"]["pinky"],
+            enablePwrPlt=gameConfig["enablePwrPlt"],
         )
 
         self.main: Tk = Tk()
-        self.main.title("Auto Pacman")
+        self.main.title("Auto Pacman Game Viewer")
 
         self.display: Display = Display(self.main)
         self.display.newGame(self.game)
 
-        self.tc: TimeController = TimeController(0.10, self.nextStep)
+        self.tc: TimeController = TimeController(gameConfig["gameSpeed"], self.nextStep)
 
         _thread.start_new_thread(self.tc.start, ())
 
@@ -46,10 +50,29 @@ class App:
         self.main.mainloop()
 
 
-if __name__ == "__main__":
-    parent: str = "out"
-    runPref: str = "RL1802_1614"
-    epCount: int = 13000
+# load neural network from config json
+def loadNeuralNet(parentFolder: str, prefix: str, ep: int) -> NeuralNet:
+    filename: str = "./{}/{}/rl_nnconf_ep{}.json".format(parentFolder, prefix, ep)
+    return NeuralNet.load(filename)
 
-    app = App("./{}/{}/rl_nnconf_ep{}.json".format(parent, runPref, epCount))
-    app.run()
+
+# load genome from config json
+def loadGenome(parentFolder: str, prefix: str, gen: int) -> Genome:
+    filename: str = "./{}/{}/ga_nnconf_ep{}.json".format(parentFolder, prefix, gen)
+    return GenomeUtils.load(filename)
+
+
+if __name__ == "__main__":
+    gameConfig: dict[str, object] = {
+        "agents": {
+            "pacman": PacmanDQLAgent(loadNeuralNet("out", "RL1802_1614", 14500)),
+            "blinky": BlinkyClassicAgent(),
+            "inky": None,
+            "clyde": None,
+            "pinky": PinkyClassicAgent(),
+        },
+        "enablePwrPlt": True,
+        "gameSpeed": 0.1,
+    }
+
+    App(gameConfig).run()
