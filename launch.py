d232605c5a1f691a32328d7c5227bea9099aa79f
lochungtin <lochungtin@gmail.com>
2022-03-31 21:26:04 +0100

    create base launch script

---
+++ launch.py
@@ -0,0 +1,16 @@
+from scripts.auto import App as Auto
+from scripts.average import App as Average
+from scripts.experiment import App as Experiment
+from scripts.play import App as Play
+
+class Launcher:
+    def __init__(self) -> None:
+        pass
+
+    def run(self) -> None:
+        return
+
+
+if __name__ == "__main__":
+    launcher: Launcher = Launcher()
+    launcher.run()
