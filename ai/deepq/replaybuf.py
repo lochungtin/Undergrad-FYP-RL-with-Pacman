b8934b4c65a927641e71cd8b25d5b37c2e84ced3
lochungtin <lochungtin@gmail.com>
2022-04-03 23:11:23 +0100

    commenting

--- ai/deepq/replaybuf.py
+++ ai/deepq/replaybuf.py
@@ -9,14 +9,18 @@ class ReplayBuffer:
         self.maxSize: int = config["rbSize"]
         self.batchSize: int = config["batchSize"]
 
+    # append new experience to replay buffer
     def append(self, s: List[List[int]], a: int, r: float, t: int, nS: List[List[int]]) -> None:
+        # remove earliest experience if full
         if len(self.buffer) == self.maxSize:
             del self.buffer[0]
 
         self.buffer.append([s, a, r, t, nS])
 
+    # return random sample of experiences
     def getSample(self) -> List[List[object]]:
         return choices(self.buffer, k=self.batchSize)
 
+    # check if buffer is ready for minibatch update
     def isReady(self):
         return len(self.buffer) > self.batchSize
